052b7adc440a91a2ce68e3a4bca0cbd0d185ef3a
toku-kawasaki <rinkodatoukyu@gmail.com>
2021-10-19 14:10:25 +0900

    can play the piano

--- webpiano/WebPiano.js
+++ webpiano/WebPiano.js
@@ -31,11 +31,12 @@ intervalIds.fill(null)              // 初期値 = null
 const pianoWrap = document.getElementById("piano-wrap")     // 鍵盤全体
 const whiteKeys = document.querySelectorAll(".white-key")   // 白鍵
 const blackKeys = document.querySelectorAll(".black-key")   // 黒鍵
+const note = ["A2","A#2","B2","C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3","C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5"]
 
 // 初期処理
 // Audioオブジェクトを作成セット
-for ( i = 0; i <= 29; i++ ){
-    let sound = new Audio( path + i + ".mp3" )
+for ( var i of note){
+    let sound = new Audio( path + i + ".wav" )
     sound.volume = 0
     pianoSounds.push(sound)
 }
