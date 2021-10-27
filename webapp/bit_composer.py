bdc3a4a867a6cee8cdbac79b588901d5ffbe7ef0
toku-kawasaki <rinkodatoukyu@gmail.com>
2021-10-27 11:24:37 +0900

    add str2mp3

--- webapp/bit_composer.py
+++ webapp/bit_composer.py
@@ -1,12 +1,14 @@
 from music21 import converter, instrument, note, chord, stream
 
 def parse_str_to_mid(str_note):
+    spl_note = str_note.split(" ")
     NoteList = []
     offset = 0.5
-    for i in str_note:
+    for i in spl_note:
         mid_note = note.Note(i,quarterLength = 1)
         mid_note.offset = offset
         mid_note.storedInstrument = instrument.Piano()
         NoteList.append(mid_note)
         offset += 0.5
-    return NoteList
+    Strmidi = stream.Stream(NoteList)
+    Strmidi.write('midi', fp=f'static\\media\\out.midi')
