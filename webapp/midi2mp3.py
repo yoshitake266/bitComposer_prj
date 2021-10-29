a234eaf5bea21b23fec2381cb42e0c94afb34dea
toku-kawasaki <rinkodatoukyu@gmail.com>
2021-10-29 09:35:42 +0900

    add midi2mp3

--- webapp/midi2mp3.py
+++ webapp/midi2mp3.py
@@ -4,4 +4,4 @@ from midi2audio import FluidSynth
 
 def output():
 	fs = FluidSynth(sound_font='default.sf2')
-	fs.midi_to_audio(f'static\\media\\out.midi', 'static\\media\\out.mp3')
+	fs.midi_to_audio(r'static\media\out.mid', r'static\media\out.mp3')
