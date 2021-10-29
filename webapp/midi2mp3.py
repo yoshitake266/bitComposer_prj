81b7bec0619c0363b3b91cdf789f18fbdac467ae
toku-kawasaki <rinkodatoukyu@gmail.com>
2021-10-29 10:10:50 +0900

    delete html

--- webapp/midi2mp3.py
+++ webapp/midi2mp3.py
@@ -3,5 +3,5 @@ from midi2audio import FluidSynth
 
 
 def output():
-	fs = FluidSynth(sound_font='default.sf2')
-	fs.midi_to_audio(f'static\\media\\out.midi', 'static\\media\\out.mp3')
+	fs = FluidSynth(sound_font='static\\media\\default.sf2')
+	fs.midi_to_audio(f'static\\media\\out.mid', 'static\\media\\out.mp3')
