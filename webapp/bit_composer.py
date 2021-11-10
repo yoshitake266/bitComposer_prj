0e9d1997229d14eb48d86b092cf96cdd15525200
toku-kawasaki <rinkodatoukyu@gmail.com>
2021-11-10 09:24:49 +0900

    change bcstr

--- webapp/bit_composer.py
+++ webapp/bit_composer.py
@@ -1,11 +1,12 @@
-from music21 import converter, instrument, note, chord, stream
+from music21 import converter, instrument, note, chord, stream,abcFormat
+import music21.abcFormat.translate as trans
 
 def parse_str_to_mid(str_note):
     spl_note = str_note.split(" ")
     NoteList = []
     offset = 0.5
     for i in spl_note:
-        mid_note = note.Note(i,quarterLength = 1)
+        mid_note = note.Note(i)
         mid_note.offset = offset
         mid_note.storedInstrument = instrument.Piano()
         NoteList.append(mid_note)
