bf87baa6936cad1b09d7d416383b28eb1711bd45
toku-kawasaki <rinkodatoukyu@gmail.com>
2021-11-26 09:59:38 +0900

    fix run.py

--- webapp/run.py
+++ webapp/run.py
@@ -49,4 +49,4 @@ def bckeysyori():
     return redirect(url_for("outputmp3"))
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True,host='0.0.0.0',port=80)
