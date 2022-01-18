cae7c40d9b10233a04da31bd730e2ab41566bf5c
toku-kawasaki <rinkodatoukyu@gmail.com>
2022-01-18 14:09:59 +0900

    add something

--- webapp/midi2mp3.py
+++ webapp/midi2mp3.py
@@ -6,3 +6,4 @@ def output():
 	path = os.path.dirname(__file__) + '/'
 	fs = FluidSynth(sound_font= path + 'static/media/default.sf2')
 	fs.midi_to_audio(path + 'static/media/out.mid', path + 'static/media/out.mp3')
+	fs.midi_to_audio(path + 'static/media/out.mid', path + 'static/media/out.wav')
