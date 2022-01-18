8e1bc31c2318c7510c5b9312070073e6a1f6fdaf
toku-kawasaki <rinkodatoukyu@gmail.com>
2022-01-18 14:18:49 +0900

    seiri

--- toku_app/midi2mp3.py
+++
@@ -1,9 +0,0 @@
-from midi2audio import FluidSynth
-import os
-
-
-def output():
-	path = os.path.dirname(__file__) + '/'
-	fs = FluidSynth(sound_font= path + 'static/media/default.sf2')
-	fs.midi_to_audio(path + 'static/media/out.mid', path + 'static/media/out.mp3')
-	fs.midi_to_audio(path + 'static/media/out.mid', path + 'static/media/out.wav')
