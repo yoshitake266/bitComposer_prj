552cf95a00f8351901ac6ec07c28edeb1fd6323f
yoshitake266 <ninten.otake7136@gmail.com>
2022-02-28 12:03:18 +0900

    delete folders

--- Winter naga/webAI/bitcomposersong/js/playSound.js
+++
@@ -1,8 +0,0 @@
-var soundID = "Thunder";
-
-function loadSound () {
-    createjs.Sound.registerSound("assets/AnyConv.com__test_output.wav", soundID);
-  }
-  function playSound () {
-    createjs.Sound.play(soundID);
-  }
--- YT_code/html/pycode.py
+++
@@ -1,3 +0,0 @@
-
-def show(data):
-	print(data)
